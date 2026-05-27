b5e99e2cc643e3f58081b00c363c80261785baba
aviaty <25710145@dntu.edu.vn>
2026-05-27 17:02:50 +0700

    Bài tập 3

---
+++ BT3/none.js
@@ -0,0 +1,27 @@
+
+ //undefined
+ let x;
+console.log(x);
+//null
+let user = null;
+console.log(user);
+//var, let, const
+//var-Có thể gán lại
+var name = "Tâm";
+name = "Aviaty";
+console.log(name);
+//Toán tử 3 ngôi
+const y = Number(prompt("Nhập giá trị y:"));
+ if (y < 56) {
+    console.log("Less than");
+ } else if (y === 56) {
+    console.log("Equal");
+ } else {
+    console.log("Greater than");
+ }
+ //Kiểm tra kiểu dữ liệu typeof
+console.log(typeof "Aviaty"); // string
+console.log(typeof 890); // number
+console.log(typeof true); // boolean
+console.log(typeof undefined); // undefined
+//
