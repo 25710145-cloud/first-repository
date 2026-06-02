83064a5c9860ea1e2833bf23f3a4dc51ef6b1502
aviaty <25710145@dntu.edu.vn>
2026-06-02 17:24:09 +0700

    BT3.1

--- BT3/none.js
+++ BT3/none.js
@@ -25,3 +25,42 @@ console.log(typeof 890); // number
 console.log(typeof true); // boolean
 console.log(typeof undefined); // undefined
 //
+//Object
+let myComputer = {
+    type: "laptop",
+    brand: "MSI",
+    os: "Windows 11",
+    graphicCard: "NVDIA",
+};
+console.log(myComputer.type);
+console.log(myComputer.brand);
+console.log(myComputer.os);
+console.log(myComputer.graphicCard);
+
+myComputer.status = "sleep";
+myComputer["it is good"] = true;
+console.log(myComputer.status);
+console.log(myComputer["it is good"]); 
+delete myComputer.brand;
+delete myComputer["graphicCard"];
+console.log(myComputer.brand);
+console.log(myComputer["graphicCard"])
+
+let letters = ["a","b"];
+letters.push("c");
+let length = letters.length;
+letters[Math.floor(length/2)] = "d";
+console.log(letters);
+let first = letters.shift();
+console.log(first);
+letters.unshift(1,2);
+console.log(letters);
+
+function sumInput() {
+      let n = prompt("Nhập vào một số: ");
+      if (n === null || n === ""|| !isFinite(n)) {
+         return 0;
+      }
+   return +n;
+}
+console.log(sumInput());
